1b16f1ce80113943be3eaf3863a75a8dd80b7e11
Amec Velasquez <amec@kalungi.com>
2024-09-25 22:02:34 -0600

    hero text animation in JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('here!')
+  Array.from(document.querySelectorAll('.text-scroller')).forEach(textScroller => {
+    const startingText = textScroller.innerText;
+    const words = textScroller.dataset.words.split(',').map(word => word.trim())
+    const wordNodes = []
+
+    const scrollDelay = 2000
+    const animationDuration = window.getComputedStyle(textScroller).getPropertyValue('transition-duration') + 1
+
+    words.forEach(word => {
+      const wordNode = document.createElement('span')
+      wordNode.innerHTML = word
+      wordNodes.push(wordNode)
+    })
+    console.log(wordNodes)
+    textScroller.innerHTML = ""
+
+    loopThroughWords(wordNodes, scrollDelay)
+    setInterval(() => {
+      loopThroughWords(wordNodes, scrollDelay)
+    }, scrollDelay*wordNodes.length)
+
+    function loopThroughWords(wordNodes, delay) {
+      wordNodes.forEach((node, i, list) => {
+        setTimeout(() => {
+          textScroller.prepend(node)
+          node.classList.add('incoming-word')
+          
+          setTimeout(() => {
+            node.classList.remove('incoming-word')
+          }, animationDuration)
+
+          setTimeout(() => {
+            list.at(i-1).classList = ''
+            list.at(i-1).remove()
+          }, animationDuration*2)
+        }, delay*i)
+      })
+    }
+  })
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,css}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        'out-back': 'cubic-bezier(0.34, 1.56, 0.64, 1)',
+      }
+    },
   },
   plugins: [],
 }
