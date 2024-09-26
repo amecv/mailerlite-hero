294f2ffd8d58ec9f87c784a28498ba3fc653d3a6
Amec Velasquez <amec@kalungi.com>
2024-09-25 22:27:59 -0600

    tweaking the animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+  // Set up the animation for any elements on the page with class "text-scroller"
   Array.from(document.querySelectorAll('.text-scroller')).forEach(textScroller => {
-    const startingText = textScroller.innerText;
     const words = textScroller.dataset.words.split(',').map(word => word.trim())
     const wordNodes = []
 
+    // How long each word should show
     const scrollDelay = 2000
-    const animationDuration = window.getComputedStyle(textScroller).getPropertyValue('transition-duration')
+    // Get the transition-duration from the css and convert it to milliseconds
+    const animationDuration = parseFloat(window.getComputedStyle(textScroller).getPropertyValue('transition-duration'))*1000
 
+    // Create span elements for each word, and put them in the 'wordNodes' array
     words.forEach(word => {
       const wordNode = document.createElement('span')
       wordNode.innerHTML = word
       wordNodes.push(wordNode)
     })
-    console.log(wordNodes)
+
+    // Clear any pre-existing text from the element
     textScroller.innerHTML = ""
 
+    //Do the thing
     loopThroughWords(wordNodes, scrollDelay)
     setInterval(() => {
       loopThroughWords(wordNodes, scrollDelay)
@@ -28,6 +34,7 @@ window.addEventListener('DOMContentLoaded', () => {
           
           setTimeout(() => {
             node.classList.remove('incoming-word')
+            list.at(i-1).classList.add('outgoing-word')
           }, animationDuration)
 
           setTimeout(() => {
